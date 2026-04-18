8cd25490b62ae0d1953cc03d8cef8e67361bedec
ABDULALEEM <abdulaleemtr@gmail.com>
2026-04-18 21:14:44 +0300

    exercise 1.4

--- part1-exrcises1/src/App.jsx
+++ part1-exrcises1/src/App.jsx
@@ -13,9 +13,9 @@ const Part = (props) => {
 const Content = (props) => {
   console.log(props)
   return <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
 }
 
@@ -26,25 +26,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const total = part1.exercises + part2.exercises + part3.exercises
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
 
   return (
     <>
       <Header course={course} />
 
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
 
       <Total total={total} />
     </>
